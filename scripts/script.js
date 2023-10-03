c2e7b189122ec148b7b2748b455d318864433147
Kamyin420 <96398575+Kamyin420@users.noreply.github.com>
2023-10-03 21:31:57 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-// JavaScript Document
-/*https://css-tricks.com/books/greatest-css-tricks/scroll-animation/*/
-
 
 
   var eersteStreepjesButton = document.querySelector("nav ul li:nth-of-type(4) button");
