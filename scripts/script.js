3602b7718699660d535a01548b2322f02ee9b706
Kamyin420 <96398575+Kamyin420@users.noreply.github.com>
2023-11-08 21:45:12 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,22 @@
   var hamburgermenu = document.querySelector("nav ul li:nth-of-type(4) button");
 hamburgermenu.onclick = hamburgermenuKlik;
 function hamburgermenuKlik() {
+  var overlay = document.getElementById("overlay");
+
   if (hamburgermenu.ariaExpanded == "false") {
-    hamburgermenu.ariaExpanded = "true"
+    hamburgermenu.ariaExpanded = "true" /* Dit is active*/
+    // add disable-scroll class to body (overflow:hidden)
+    overlay.style.display = "block";
+    document.body.classList.add("disable-scroll");
     
   }
   else {
     hamburgermenu.ariaExpanded = "false"
+    // REMOVE disable-scroll class from body
+    document.body.classList.remove("disable-scroll");
+
+    
+    overlay.style.display = "none";
   }
 }
 
@@ -16,8 +26,7 @@ function hamburgermenuKlik() {
 
 //////////////// Voor scroll animatie ///////////////// https://css-tricks.com/books/greatest-css-tricks/scroll-animation////
 
-window.addEventListener('scroll', () => {
-  document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+// window.addEventListener('scroll', () => {
+//   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+// }, false);
 
-onclick
